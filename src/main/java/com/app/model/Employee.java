aff40512f07f6f329990f957212ba5080d1c8f3e
laxman <laxman@laxman-Compaq-15-Notebook-PC>
2019-05-09 16:34:10 +0530

    class added

--- src/main/java/com/app/model/Employee.java
+++ src/main/java/com/app/model/Employee.java
@@ -3,4 +3,14 @@ package com.app.model;
 public class Employee {
 	private Integer ab;
 	private String abc;
+	
+	private String address ;
+
+	public Employee(Integer ab, String abc, String address) {
+		super();
+		this.ab = ab;
+		this.abc = abc;
+		this.address = address;
+	}
+	
 }
